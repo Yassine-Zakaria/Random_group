284b906bddf1a1863a978ec0e9b3eaef71ab24ec
Yassine <yassine.zakaria66@gmail.com>
2023-02-01 15:29:24 +0100

    ma modification

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Random_group
-Ce repository est pour un TP
+Ce repository est pour un TP oui c'est deja noté
+Mr sachez que jre viens demodifier le fichier md sur la branche new_version comme vous avz demander.
+Merci infinement pour ce fameux TP.
--- random.py
+++ random.py
@@ -1,5 +1,5 @@
-import random
-ma_list =["anne","aline","gros","eve","armand","yves","elv","allo","sonia","luc","marc","jules","kevin"]
+import random 
+ma_list =["anne","aline","gros","eve","armand","yves","elv","allo","sonia","luc","marc","jules","kevin","Yassine","Hamza","issa"]
 #this will contain an occurence of our list
 maListOc = ma_list
 #this list will contain our random list
@@ -10,13 +10,13 @@ for i in ma_list:
 #This condition will avoid errors when n is greater than 4
     if(len(ma_list)<=5):
         break
-    random_list = random.sample(ma_list, 6)
+    random_list = random.sample(ma_list, 5)
     #then we remove data already randomize in our list, but the complexity is high for this little program
     for element in random_list:
         ma_list.remove(element)
-    print("Goupe N°:",groupe)
+    print("Goupe N°:",groupe, random_list)
     #and we finally print our randomized list
-    print(random_list)
+    print()
     print("______________________________")
     groupe += 1
-print(maListOc)
+print("Goupe N°:",groupe,maListOc)# j'ais juste ajouter ça et à la ligne 10 j'ais juste modifier le 6 par 5
